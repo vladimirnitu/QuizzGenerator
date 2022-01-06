af0657664c87b3705ab013eb5002f3ea774eb75d
impulse1997 <nituvladimir2@gmail.com>
2022-01-06 21:32:52 +0200

    Update user.ts

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -3,11 +3,11 @@ import * as userMiddleware from '../middleware/user'
 const router = Router();
 const multer = require('multer')
 var uploadEmpty = multer()
-router.post('/register/user',uploadEmpty.none(), userMiddleware.createUserWithAlldetailsFromRegister)
+router.post('/register/user', uploadEmpty.none(), userMiddleware.createUserWithAlldetailsFromRegister)
 
-router.post('/login/user',uploadEmpty.none(), userMiddleware.loginUser)
+router.post('/login/user', uploadEmpty.none(), userMiddleware.loginUser)
 
 router.get('/getUsers', userMiddleware.getAllUsers)
 
-router.get('/getUser', userMiddleware.findOneUser)
+router.post('/getUser', userMiddleware.findOneUser)
 export default router
